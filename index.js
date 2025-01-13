c698e477b01dc35c3b00a21b376a5f34eea2d39a
Saparmurat <saparmurat@protonmail.com>
2025-01-13 16:02:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log("Hello World");
+});
+
+app.get('/', (request, response) => {
+    response.send("Hello from Node.js/Exress API!")
+});
