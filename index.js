33934e973569bb393d6536e83d88cfe68dcefa32
Saparmurat <saparmurat@protonmail.com>
2025-02-02 23:37:33 +0600

    add db, use router

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoute = require("./routes/product.router.js");
+
 const app = express();
 
+mongoose
+    .connect("mongodb://mongouser:mongopass@127.0.0.1:27017/admin", {
+        authSource: "admin",
+    })
+    .then(() => {
+        console.log("MongoDB connected!");
+        app.listen(3000, () => {
+            console.log("Hello World");
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
 
-app.listen(3000, () => {
-    console.log("Hello World");
-});
-
-app.get('/', (request, response) => {
-    response.send("Hello from Node.js/Exress API!")
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/products", productRoute);
